6f44c1d74576592a7185c454de35f5907637f30f
Elliot-Wadge <75158330+Elliot-Wadge@users.noreply.github.com>
2021-03-15 19:36:39 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Curve_fit_plus',
+    url='https://github.com/Elliot-Wadge/curve_fit_plus',
+    author='Elliot Wadge',
+    author_email='ewadge@sfu.ca',
+    # Needed to actually package something
+    packages=['curve_fit_plus'],
+    # Needed for dependencies
+    install_requires=['numpy,scipy,matplotlib'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='E',
+    description='a curve fitting ',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
+
